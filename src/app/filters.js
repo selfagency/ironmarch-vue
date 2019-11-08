624d524d7f9ad81adfd7f295340ac5529ab98562
The Jewish Worker <hello@jewishworker.org>
2019-11-08 05:42:45 -0500

    add word splitting

--- src/app/filters.js
+++ src/app/filters.js
@@ -3,6 +3,25 @@ import stripHtml from 'string-strip-html'
 import truncate from 'truncate'
 import { StripChar } from 'stripchar'
 
+const chunk = (str, n) => {
+  let out = [],
+    i,
+    len
+  for (i = 0, len = str.length; i < len; i += n) {
+    out.push(str.substr(i, n))
+  }
+  return out
+}
+
+const breakup = str => {
+  str = str.split(' ')
+  str = str.map(s => {
+    if (s.length > 12) s = chunk(s, 12).join('\u00AD')
+    return s
+  })
+  return str.join(' ')
+}
+
 const filters = Vue => {
   Vue.filter('capitalize', str => {
     if (!str) return ''
@@ -16,7 +35,7 @@ const filters = Vue => {
 
   Vue.filter('truncate', (str, ln) => {
     if (!str) return ''
-    return truncate(StripChar.RSspecChar(stripHtml(str), '\u00AD'), ln)
+    return truncate(breakup(StripChar.RSspecChar(stripHtml(str), '\u00AD')), ln)
   })
 
   Vue.filter('dateConv', date => {
