6316232f3ee1e30f4676ca4c8ce7d9ee79148ffb
The Jewish Worker <hello@jewishworker.org>
2019-11-08 04:05:33 -0500

    remove extraneous modules

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
-    }
-  },
-
-  chainWebpack: config => {
-    config.module
-      .rule('svg-sprite')
-      .use('svgo-loader')
-      .loader('svgo-loader')
-  }
+  // pluginOptions: {
+  //   i18n: {
+  //     locale: 'en',
+  //     fallbackLocale: 'en',
+  //     localeDir: 'locales',
+  //     enableInSFC: false
+  //   }
+  // },
+  // chainWebpack: config => {
+  //   config.module
+  //     .rule('svg-sprite')
+  //     .use('svgo-loader')
+  //     .loader('svgo-loader')
+  // }
 }
