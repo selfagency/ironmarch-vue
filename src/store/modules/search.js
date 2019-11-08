86b1eaab2e1fcffe51b870c8cb259c477c7903dc
The Jewish Worker <hello@jewishworker.org>
2019-11-08 12:08:52 -0500

    o🤬

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -21,7 +21,7 @@ const mutations = {
     state.params.terms = terms
   },
   RESULTS_ADD(state, result) {
-    result.length ? state.results.push(...result) : state.results.push(result)
+    result.length ? state.results.push(...result) : state.results.push({})
   },
   RESULTS_DEL(state) {
     state.results = []
