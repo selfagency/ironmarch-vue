9f80fd6ce0766680be867f467397cb5698aaa560
The Jewish Worker <hello@jewishworker.org>
2019-11-08 15:29:32 -0500

    kill me

--- src/store/modules/content.js
+++ src/store/modules/content.js
@@ -36,7 +36,7 @@ const actions = {
         return item.hash === hash
       })[0]
 
-      if (!Object.values(content).length) {
+      if (!content) {
         content = await get(method, params)
         content.hash = hash
         dispatch('addContent', { method, content })
