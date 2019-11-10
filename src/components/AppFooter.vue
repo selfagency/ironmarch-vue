d5a7ec92d5da372d5072ff69114613b9004d84c5
The Jewish Worker <hello@jewishworker.org>
2019-11-10 02:32:51 -0500

    update footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,7 +3,17 @@
     <div class="credits">
       <small>
         Built by <a href="https://jewishworker.org">The Jewish Worker</a> ·
-        <a href="https://archive.org/details/iron_march_201911">Source data</a>
+        <a href="https://archive.org/details/iron_march_201911">Source data</a> ·
+        <a href="https://gitlab.com/jewishworker">Source code</a>
+        <br />
+        This website does not collect or track any information about its visitors.
+        <br /><br />
+        <a href="https://twitter.com/jewishworker" target="_blank">
+          <img
+            src="https://img.shields.io/twitter/follow/jewishworker?style=social"
+            alt="Follow Jewish Worker on Twitter"
+          />
+        </a>
       </small>
     </div>
   </footer>
