9f372b6210eb4bca273525ee9823e61231401b56
The Jewish Worker <hello@jewishworker.org>
2019-11-09 23:25:14 -0500

    new data integration

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,19 +1,29 @@
 <template>
   <section v-if="user && Object.values(user).length" id="meta" class="row">
     <div class="column column-75">
-      <div v-if="user.lookup">
-        <div v-if="user.lookup.contactInfo"><strong>Real Name:</strong> {{ user.lookup.contactInfo.fullName }}</div>
+      <div v-if="lookup">
+        <div v-if="lookup.fullName"><strong>Real Name:</strong> {{ lookup.fullName }}</div>
+        <div>
+          <span v-if="lookup.details.gender"><strong>Gender:</strong> {{ lookup.details.gender }}</span>
+          <span v-if="lookup.details.gender && lookup.details.age"> · </span>
+          <span v-if="lookup.details.age"><strong>Age:</strong> {{ lookup.details.age }}</span>
+        </div>
         <div v-if="user.name"><strong>Username:</strong> {{ user.name }}</div>
         <div v-if="user.email"><strong>Email:</strong> {{ user.email }}</div>
-        <ul class="lookup-social">
-          <li v-for="(social, key) in user.lookup.socialProfiles" :key="key">
-            <strong>{{ social.typeName }}: </strong>
-            <a :href="social.url" target="_blank">{{ social.username || social.id }}</a>
-          </li>
-        </ul>
+        <div v-if="Object.values(lookup.details.profiles).length">
+          <strong>Social profiles:</strong>
+          <ul class="lookup-social">
+            <li v-for="(social, key) in lookup.details.profiles" :key="key">
+              <a :href="social.url" target="_blank">{{ social.service | capitalize }}</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div v-if="user.id"><strong>ID:</strong> {{ user.id }}</div>
-      <div v-if="user.ip"><strong>IP:</strong> {{ user.ip }}</div>
+      <div>
+        <span v-if="user.id"><strong>ID:</strong> {{ user.id }}</span>
+        <span v-if="user.id && user.ip"> · </span>
+        <span v-if="user.ip"><strong>IP:</strong> {{ user.ip }}</span>
+      </div>
       <div v-if="user.geo">
         <strong>Geolocation:</strong> {{ user.geo.city }}, {{ user.geo.region_name }}, {{ user.geo.country_code }}
       </div>
@@ -24,8 +34,8 @@
       <div v-if="user.salt"><strong>Password Salt:</strong> {{ user.salt }}</div>
     </div>
     <div class="column column-25">
-      <div v-if="user.lookup && user.lookup.photos">
-        <img id="user-photo" :src="user.lookup.photos[0].url" />
+      <div v-if="user.lookup && lookup.details.photos">
+        <img id="user-photo" :src="lookup.details.photos[0].value" />
       </div>
     </div>
   </section>
@@ -40,6 +50,13 @@ export default {
       default() {
         return {}
       }
+    }
+  },
+  computed: {
+    lookup() {
+      const lookup = JSON.parse(this.user.lookup)
+      Console.log(lookup)
+      return lookup
     }
   },
   mounted() {
